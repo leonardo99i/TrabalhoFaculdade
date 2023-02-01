53307c3865a3709db0f8252b4e637f322f42d7d3
Rian Xavier <rianalvesxvr@gmail.com>
2023-02-01 15:59:43 -0300

    erro direcionamento para tela de login

--- Assets/JS/inicio.js
+++ Assets/JS/inicio.js
@@ -5,7 +5,7 @@ let msgLogado = document.querySelector('#msgLogado');
 
 if(localStorage.getItem('token') == null){
     alert('Você precisa estar logado para acessar essa página!')
-    window.location.href = '../../index.html'
+    window.location.href = '../index.html'
 }else{
     msgLogado.innerHTML = 'Bem Vindo,  '+ userLogado.nome
 }
